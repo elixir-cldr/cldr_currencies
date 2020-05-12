a11cb1f74c7395786bdb5f998a105ace83019e0b
Kip Cole <kipcole9@gmail.com>
2020-05-12 23:08:54 +0800

    Reelase 2.5.0

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule CldrCurrencies.MixProject do
 
   defp deps do
     [
-      {:ex_cldr, "~> 2.14-rc"},
+      {:ex_cldr, "~> 2.14"},
       {:jason, "~> 1.0", optional: true},
       {:ex_doc, "~> 0.18", only: [:dev, :release, :test], optional: true},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
