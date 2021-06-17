8685ade2718ad66cc4c9b223d11806a973c162bc
Kip Cole <kipcole9@gmail.com>
2021-06-17 13:51:02 +0800

    Fix test

--- test/cldr_currencies_test.exs
+++ test/cldr_currencies_test.exs
@@ -40,8 +40,8 @@ defmodule Cldr.Currency.Test do
   end
 
   test "names with annotations are intact" do
-    assert Cldr.Currency.strings_for_currency(:USN, "en", MyApp.Cldr) ==
-             ["us dollars (next day)", "us dollar (next day)", "usn"]
+    assert Cldr.Currency.strings_for_currency(:USN, "en", MyApp.Cldr) |> Enum.sort ==
+      ["us dollar (next day)", "us dollars (next day)", "usn"]
   end
 
   test "currency strings is a map" do
